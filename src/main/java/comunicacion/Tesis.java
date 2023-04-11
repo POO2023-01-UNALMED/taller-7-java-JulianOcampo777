c35e852f4964a072c7dd9ab4b763446504f6bd95
JulianOcampo777 <124713530+JulianOcampo777@users.noreply.github.com>
2023-04-10 19:57:17 -0500

    TS1-31

--- src/main/java/comunicacion/Tesis.java
+++ src/main/java/comunicacion/Tesis.java
@@ -1,7 +1,66 @@
 package comunicacion;
 
-public class Tesis {
+public class Tesis extends Escrito {
 	private String idea;
+	private String[] argumentos;
+	private String conclusion;
+	private String referencias;
+	private String interpretacion;
+	
+	
+	public Tesis(String origen, String titulo, String autor, int paginas, String idea, String[] argumentos, String conclusion, String referencias, String interpretacion) {
+		super(origen, titulo, autor, paginas);
+		this.idea=idea;
+		this.argumentos=argumentos;
+		this.conclusion=conclusion;
+		this.referencias=referencias;
+		this.interpretacion=interpretacion;
+	}
+	
+	
+	public String[] getArgumentos() {
+		return argumentos;
+	}
+	public void setArgumentos(String[] argumentos) {
+		this.argumentos = argumentos;
+	}
+	public String getReferencias() {
+		return referencias;
+	}
+	public void setReferencias(String referencias) {
+		this.referencias = referencias;
+	}
+	public String getIdea() {
+		return idea;
+	}
+	public void setIdea(String idea) {
+		this.idea = idea;
+	}
+	public String getInterpretacion() {
+		return interpretacion;
+	}
+	public void setInterpretacion(String interpretacion) {
+		this.interpretacion = interpretacion;
+	}
+	public String getConclusion() {
+		return conclusion;
+	}
+	public void setConclusion(String conclusion) {
+		this.conclusion = conclusion;
+	}
+	
+	
+	
+	public int palabrasTotales(int totales) {
+		return super.getPaginas()*totales*5;
+	}
+	public String interpretacion() {
+		return interpretacion;
+	}
+	public String toString() {
+		return super.getOrigen()+"\n"+super.getTitulo()+"\n"+super.getAutor() +"\n"+super.getPaginas()+"\n"+getIdea()+"\n"+getArgumentos()+"\n"+getConclusion()+"\n"+getReferencias();
+	}
+	
 	
 
 }
