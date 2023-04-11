0811886cabf097308078627c8304ec96dc24b378
JulianOcampo777 <124713530+JulianOcampo777@users.noreply.github.com>
2023-04-10 20:25:03 -0500

    TS1-34

--- src/main/java/comunicacion/Fabula.java
+++ src/main/java/comunicacion/Fabula.java
@@ -15,8 +15,8 @@ public class Fabula extends Escrito {
 	public String getEnsenanza() {
 		return ensenanza;
 	}
-	public void setEnsenanza(String enseñanza) {
-		this.ensenanza = enseñanza;
+	public void setEnsenanza(String ensenanza) {
+		this.ensenanza = ensenanza;
 	}
 	public String getInterpretacion() {
 		return interpretacion;
